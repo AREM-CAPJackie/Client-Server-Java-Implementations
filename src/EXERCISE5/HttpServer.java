6f5157cba2b9fbfac0a7b605c9f3768b7a0872d1
monitor monitor <monitor@labinfo.is.escuelaing.edu.co>
2018-02-26 18:31:32 -0500

    Exercise 5 finished

--- src/EXERCISE5/HttpServer.java
+++ src/EXERCISE5/HttpServer.java
@@ -36,19 +36,32 @@ public class HttpServer {
                 break;
             }
         }
-        outputLine = "<!DOCTYPE html>"
-        + "<html>"
-        + "<head>"
-        + "<meta charset=\"UTF-8\">"
-        + "<title>Title of the document</title>\n"
-        + "</head>"
-        + "<body>"
-        + "My Web Site"
-        + "</body>"
-        + "</html>" + inputLine;
-        out.println(outputLine);
-       
-
+        outputLine = "HTTP/1.1 200 OK \n"
+            +"Content-Type: text/html; charset=utf-8\n"
+            +"Cache-Control: public, max-age=60, s-maxage=300\n"
+            +"Vary: Accept-Encoding\n"
+            +"Content-Encoding: raw\n"
+            +"Server: DPS/1.3.5\n"
+            +"X-SiteId: 2000\n"
+            +"Set-Cookie: dps_site_id=2000; path=/\n"
+            +"ETag: a1be084ec13bc207c17c49955f875fab\n"
+            +"Date: Mon, 26 Feb 2018 21:47:54 GMT\n"
+            +"Connection: keep-alive\n"
+            +"Transfer-Encoding: chunked\n" 
+            +"\n"
+            +"3c6f"
+            + "<!DOCTYPE html>"
+            + "<html>"
+            + "<head>"
+            + "<meta charset=\"UTF-8\">"
+            + "<title>Title of the document</title>\n"
+            + "</head>"
+            + "<body>"
+            + "My Web Site"
+            + "</body>"
+            + "</html>"                                    
+            + inputLine;
+        out.println(outputLine);       
         out.close();
         in.close();
         clientSocket.close();
