0832b1c357327daef68a1d7c9ebcbd320888c1ed
unknown <2110805@labinfo.is.escuelaing.edu.co>
2018-03-02 14:29:47 -0500

    Sequential Http Server

--- src/EXERCISE2/URLDataHtml.java
+++ src/EXERCISE2/URLDataHtml.java
@@ -1,26 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package EXERCISE2;
 
 import java.io.BufferedReader;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.PrintStream;
 import java.io.PrintWriter;
 import java.net.URL;
 
-/**
- *
- * @author 2110805
- */
 public class URLDataHtml {
     public static void main(String[] args) throws Exception {
-        //FileOutputStream salida = new FileOutputStream("documentacion.html");
-        //PrintStream p = new PrintStream(salida);
         PrintWriter writer = new PrintWriter("result.html");
                
         URL google = new URL("http://www.google.com/");
